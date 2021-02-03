9d754653e002114326634c8b9894c2351bf87845
Guillaume <guillaume_proulx@outlook.com>
2021-02-03 12:00:19 -0500

    Test automatique done
    Ajout de l'information des tests pendant la simulation

--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -16,4 +16,20 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
     GENERATED_BODY()
 public:
     virtual void Tick(float deltaTime) override;
+
+    void IncrementAIDeathCount();
+    void IncrementAICollectibleCount();
+
+private:
+
+    void DisplayTestInformation(float delatTime);
+
+    // Information about the AI automatic test
+    int m_numberPickUp = 0;
+    int m_numberAIDeath = 0;
+    float m_timer = 0.f;
+
+    // Determine the test's lenght in sec
+    UPROPERTY(EditAnywhere)
+        float m_testPeriod = 60;
 };
