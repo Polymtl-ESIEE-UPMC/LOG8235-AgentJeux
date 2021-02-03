be422f41b32e52087e2b0c750e6784c8f874850c
Pierre-Emmanuel Savoie <savoiepe@gmail.com>
2021-02-03 12:08:45 -0500

    ajout description des paramètres dans unreal

--- Source/SoftDesignTraining/SDTAIController.h
+++ Source/SoftDesignTraining/SDTAIController.h
@@ -21,12 +21,23 @@ public:
     bool DetectDeathZone(APawn* pawn, UWorld* World, FVector viewDirection);
     TArray<FHitResult> CollectVisibleElements(APawn* pawn, UWorld* World, FVector viewDirection);
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
+    /** Movement speed of the agent. 
+    Needs to be a value between 0 and 1. 
+    0 : The agent doesn't move. 
+    1 : The agent moves at full speed. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = "0.0", ClampMax = "1.0") )
     float movementSpeed = 0.5f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
+    /** View distance of the agent.
+    If the player is further than the view distance, the player will not be detected.
+    Needs to be greater than 0.*/
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = "0.0"))
     float viewDistance = 1000;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI)
+    /** View angle of the player.
+    If the angle between the agent view direction and the direction to the player is bigger than the view angle, the player will not be detected.
+    Needs to be between 0 and 180.
+    At a value of 180, the pawn will all around him.*/
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AI, meta = (ClampMin = "0.0", ClampMax = "180.0"))
     float viewAngle = 40;
 };
