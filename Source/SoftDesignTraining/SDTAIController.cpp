516ce068710a8ac0b112b2e36c1a962a759a99a1
Guillaume <guillaume_proulx@outlook.com>
2021-02-04 01:21:16 -0500

    Change to show information in play

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -20,25 +20,23 @@ void ASDTAIController::IncrementAICollectibleCount() {
 
 void ASDTAIController::DisplayTestInformation(float delatTime) {
 
-	//Verify is editor is simulating 
-	if (GEditor->IsSimulatingInEditor()) {
-		//Show stats and verify if GEngine is not 0
-		if (GEngine) {
-			GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Green, FString::Printf(TEXT("------------------------------"), *(GetPawn()->GetName())));
-			GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red, FString::Printf(TEXT("Total of AI's Death: %s"), *FString::FromInt(m_numberAIDeath)));
-			GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red, FString::Printf(TEXT("Total of AI's pickup: %s"), *FString::FromInt(m_numberPickUp)));
-			GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red, FString::Printf(TEXT("Timer: %s"), *FString::FromInt(m_timer)));
-			GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Green, FString::Printf(TEXT("------------- %s -------------"), *(GetPawn()->GetName())));
+	//Show stats and verify if GEngine is not 0
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Green, FString::Printf(TEXT("------------------------------"), *(GetPawn()->GetName())));
+		GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red, FString::Printf(TEXT("Total of AI's Death: %s"), *FString::FromInt(m_numberAIDeath)));
+		GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red, FString::Printf(TEXT("Total of AI's pickup: %s"), *FString::FromInt(m_numberPickUp)));
+		GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Red, FString::Printf(TEXT("Timer: %s"), *FString::FromInt(m_timer)));
+		GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Green, FString::Printf(TEXT("------------- %s -------------"), *(GetPawn()->GetName())));
 
-			m_timer += delatTime;
+		m_timer += delatTime;
 
-			//Reset stats if Period is over
-			if (m_timer >= m_testPeriod) {
-				m_numberAIDeath = 0;
-				m_numberPickUp = 0;
-				m_timer = 0;
-			}
+		//Reset stats if Period is over
+		if (m_timer >= m_testPeriod) {
+			m_numberAIDeath = 0;
+			m_numberPickUp = 0;
+			m_timer = 0;
 		}
-	}	
+	}
+		
 }
 
